100a1bfc316622d82618a5d17813f890ba185cc3
Matthew Sullivan <msullivan@logmein.com>
2018-07-18 16:26:46 -0400

    fix tracking

--- app/tracking.js
+++ app/tracking.js
@@ -2,21 +2,30 @@ import { SEGMENT_WRITE_KEY } from 'react-native-dotenv'
 import Analytics from 'analytics-react-native';
 import DeviceInfo from 'react-native-device-info';
 
-const analytics = new Analytics((__DEV__) ? "I8QHtyds2oHFGevqRV02t2IiEioYhWx" : "0IrQ4C4Ik64KABwz5lYXr1ohMQKWfV2V");
+const analytics = new Analytics((__DEV__) ? "0IrQ4C4Ik64KABwz5lYXr1ohMQKWfV2V" : "jI8QHtyds2oHFGevqRV02t2IiEioYhWx");
 
 export const DEVICE_ID = DeviceInfo.getUniqueID();
+var _userId;
 
 //https://segment.com/docs/sources/server/node/#track
 export function track(event, properties) {
-	analytics.track({
-		anonymousId: DEVICE_ID,
+	const data = {
 		event, 
 		properties
-	});
+	};
+
+	if(_userId){
+		data['userId'] = _userId;
+	}else{
+		data['anonymousId'] = DEVICE_ID;
+	}
+
+	analytics.track(data);
 }
 
 //https://segment.com/docs/sources/server/node/#identify
 export function identify(userId, traits) {
+	_userId = userId;
 	analytics.identify({
 		userId,
 		traits
